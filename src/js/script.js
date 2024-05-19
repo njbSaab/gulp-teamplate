f0844b7a341a2699f14909d66a00bc2cf03aec8f
Admin <1q.expert@gmail.com>
2024-05-20 01:46:45 +0300

    adaptive anim done

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,29 @@
-const buttons = document.querySelectorAll(".btn-regular");
+var buttons = document.querySelectorAll('.btn-regular');
 
-buttons.forEach((button) => {
-  button.onclick = function (e) {
-    let x = e.clientX - e.target.offsetLeft;
-    let y = e.clientY - e.target.offsetTop;
-    let ripple = document.createElement("span");
-    ripple.style.left = `${x}px`;
-    ripple.style.top = `${y}px`;
-    this.appendChild(ripple);
-    setTimeout(() => {
-      ripple.remove();
-    }, 600);
-  };
+buttons.forEach(function(b) {
+  b.addEventListener('click', createRipple);
 });
+
+function createRipple(e) {
+  if (this.getElementsByClassName('ripple').length > 0) {
+    this.removeChild(this.getElementsByClassName('ripple')[0]); // Удаляем существующий ripple
+  }
+
+  var circle = document.createElement('div');
+  circle.classList.add('ripple');
+  this.appendChild(circle);
+
+  var d = Math.max(this.clientWidth, this.clientHeight);
+  circle.style.width = circle.style.height = d + 'px';
+
+  var rect = this.getBoundingClientRect();
+  var x = e.clientX - rect.left - d / 2;
+  var y = e.clientY - rect.top - d / 2;
+
+  circle.style.left = x + 'px';
+  circle.style.top = y + 'px';
+
+  setTimeout(function() {
+    circle.remove();
+  }, 600); // Длительность анимации совпадает с CSS
+}
